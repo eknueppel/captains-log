859ae213d64fbb7c804da685fc28aedc6d4083d3
Edward Knueppel <Edward.Knueppel@zywave.com>
2024-06-08 21:03:08 +0000

    Backup working entry

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # captains-log
-A Streamlit Captain's Log
+## A Streamlit Captain's Log
+
+
+Run the app with the following command:
+```terminal
+streamlit run main.py
+```
+
+Requirement:
+- streamlit
+- pandas
+
+Development:
+- run Docker
+- open in devcontainer
--- main.py
+++ main.py
@@ -18,13 +18,17 @@ def submit_entry():
     st.session_state.new_entry = st.session_state.entry_textarea
     st.session_state.entry_textarea = ''
 
+def add_entry_backup(): # TODO how should this be reapplied? Is there a saved bool? Does it clear on save?
+    with open("backup.txt", "w") as f:
+        f.write(st.session_state.entry_textarea)
+
 def main():
     st.title("Captain's Log")
     st.logo('father.jpg')
     if 'df' not in st.session_state:
         st.session_state.df = load_dataframe()
     if "tags" not in st.session_state:
-            st.session_state.tags = ['Log', 'AE Standup', 'RO Standup', 'Task', 'Reminder']
+            st.session_state.tags = ['Log', 'AE Standup', 'RO Standup', 'Task', 'Reminder', 'Release']
     
     ADD = "Add Log Entry"
     LOGS_RAW = "View Raw Logs"
@@ -32,21 +36,22 @@ def main():
     CHARTS = "View Charts"
 
     navigation_views = [ADD, LOGS_RAW, LOGS, CHARTS]
-    navigation_choice = st.sidebar.selectbox("Menu", navigation_views)
-
+    navigation_choice = ''
+    with st.sidebar:
+        navigation_choice = st.selectbox("Menu", navigation_views)
+    
     if navigation_choice == ADD:
         st.subheader("Add a new Log. . .")
         date = st.date_input("Select date", dt.datetime.now(), key="date_input")
+        selected_tags = st.multiselect("Select tags", st.session_state.tags, default='Log', key="tags_multiselect")
         
-        # Input for new LogEntry
         if "entry_textarea" not in st.session_state:
             st.session_state.entry_textarea = ""
         if "new_entry" not in st.session_state:
             st.session_state.new_entry = ""
-
-        st.text_area('Entry', height=20, label_visibility="collapsed", key="entry_textarea")
+        st.text_area('Entry', height=200, label_visibility="collapsed", key="entry_textarea", on_change=add_entry_backup)
+        
         st.write(f"You wrote {len(st.session_state.new_entry)} characters.")
-        selected_tags = st.multiselect("Select tags", st.session_state.tags, default='Log', key="tags_multiselect")
 
         if st.button("Add", key="add_button", on_click=submit_entry):
             st.session_state.df.loc[len(st.session_state.df)] = pd.Series(
